9d58794622c3e2ad9742ab2324b8536c9ea380a4
Toshiro-Morishita <toshimori1234@gmail.com>
2019-03-02 13:23:38 +0900

    課題を全て実装する

--- main.js
+++ main.js
@@ -20,19 +20,11 @@
 
       <div class="product-info">
         <h1>{{ title }}</h1>
-        <p v-if="inStock">In Stock On Sale!</p>
-        <p v-else>Out of Stock</p>
-        <p>Shipping: {{ shipping }}</p>
+        <p v-if="inStock">In Stock On</p>
+        <p v-else :class="{ outOfClass: !inStock }">Out of Stock</p>
+        <p>{{ sale }}</p>
 
-        <div class="array">
-        <ul class="detail">
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <ul class="size">
-          <li v-for="size in sizes">{{ size }}</li>
-        </ul>
-        </div>
+        <info-tabs :shipping="shipping" :details="details" :sizes="sizes"></info-tabs>
 
         <div v-for="(variant, index) in variants"
              :key="variant.variantId"
@@ -40,7 +32,6 @@
              :style="{ backgroundColor: variant.variantColor }"
              @mouseover="updateProduct(index)">
         </div>
-
 
         <button v-on:click="addToCart"
                 :disabled="!inStock"
@@ -72,12 +63,14 @@
             variantColor: "green",
             variantImage: './assets/vmSocks-green-onWhite.jpg',
             variantQuantity: 10,
+            variantOnSale: true
           },
           {
             variantId: 2235,
             variantColor: "blue",
             variantImage: './assets/vmSocks-blue-onWhite.jpg',
-            variantQuantity: 0
+            variantQuantity: 0,
+            variantOnSale: false
           }
         ],
         reviews: []
@@ -104,6 +97,12 @@
       },
       inStock() {
         return this.variants[this.selectedVariant].variantQuantity
+      },
+      sale() {
+        if (this.variants[this.selectedVariant].variantOnSale) {
+          return this.brand + ' ' + this.product + ' are on sale!'
+         }
+          return this.brand + ' ' + this.product + ' are not on sale'
       },
       shipping() {
         if (this.premium) {
@@ -151,6 +150,16 @@
       </select>
     </p>
 
+    <p>Would you recommend this product?</p>
+    <label>
+      Yes
+      <input type="radio" value="Yes" v-model="recommend"/>
+    </label>
+    <label>
+      No
+      <input type="radio" value="No" v-model="recommend"/>
+    </label>
+
     <p>
       <input type="submit" value="submit">
     </p>
@@ -162,26 +171,30 @@
         name: null,
         review: null,
         rating: null,
+        recommend: null,
         errors: []
       }
     },
     methods: {
       onSubmit() {
-        if (this.name && this.review && this.rating) {
+        if (this.name && this.review && this.rating && this.recommend) {
           let productReview = {
             name: this.name,
             review: this.review,
-            rating: this.rating
+            rating: this.rating,
+            recommend: this.recommend
           }
           eventBus.$emit('review-submitted', productReview)
           this.name = null
           this.review = null
           this.rating = null
+          this.recommend = null
         }
         else {
           if(!this.name) this.errors.push("Name required.")
           if(!this.review) this.errors.push("Review required.")
           if(!this.rating) this.errors.push("Rating required.")
+          if(!this.recommend) this.errors.push("Recommend required.")
         }
       }
     }
@@ -227,6 +240,58 @@
         selectedTab: 'Reviews'
       }
     }
+  })
+
+  Vue.component('info-tabs', {
+    props: {
+      shipping: {
+        required: true
+      },
+      details: {
+        type: Array,
+        required: true
+      },
+      sizes: {
+        type: Array,
+        required: true
+      }
+    },
+    template:`
+      <div>
+
+        <ul>
+          <span class="tab"
+                :class="{ activeTab: selectedTab === tab }"
+                v-for="(tab, index) in tabs"
+                @click="selectedTab = tab"
+                :key="tab"
+          >{{ tab }}</span>
+        </ul>
+
+        <div v-show="selectedTab === 'Shipping'">
+          <p>{{ shipping }}</p>
+        </div>
+
+        <div v-show="selectedTab === 'Details'">
+          <div class="array">
+            <ul class="detail">
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <ul class="size">
+              <li v-for="size in sizes">{{ size }}</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+      `,
+      data() {
+        return {
+          tabs: ['Shipping', 'Details'],
+          selectedTab: 'Shipping'
+        }
+      }
   })
 
   let app = new Vue({
